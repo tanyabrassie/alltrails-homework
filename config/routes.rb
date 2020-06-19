6998d78532a8b60e723c3fe094afd2575a5f4f43
Tanya Brassie <tanyabrassie@tanyascmachine2.home>
2020-06-19 13:38:30 -0400

    Creating restaurants controller, setting up react

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'restaurants#index'
 end
