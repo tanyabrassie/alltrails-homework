d02a53ad6ae6b569e23df57d8d257b9b6fd7d13e
Tanya Brassie <tanyabrassie@tanyascmachine2.home>
2020-06-20 13:20:56 -0400

    Including prop types for web pack

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../components/home/Index';
 
