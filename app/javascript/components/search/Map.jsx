4756ea8d919064e0d47ea27153afe614601bf699
Tanya Brassie <tanyabrassie@tanyascmachine2.home>
2020-06-22 11:44:10 -0400

    Map responsive styles

--- app/javascript/components/search/Map.jsx
+++ app/javascript/components/search/Map.jsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import {useRef, useEffect} from 'react';
 import { Map as GoogleMap, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import {useMediaQuery} from 'react-responsive';
 
 const containerStyles = {
   height: 'calc(100vh - 65px)',
-  width: 'calc(100vw - 350px)',
+  width:  'calc(100vw - 350px)',
   position: 'relative',
+};
+
+const mobileStyles = {
+  height: 'calc(100vh - 65px)',
+  width:  '100%',
 };
 
 const Map = (props) => {
   const mapsRef = useRef(null);
+  const isDesktop = useMediaQuery({ minWidth: 578 });
 
   const getMapCenter = () => {
     const center = mapsRef.current.map.getCenter();
@@ -34,7 +41,7 @@ const Map = (props) => {
       streetViewControl={false}
       fullscreenControl={false}
       mapTypeControl={false}
-      containerStyle={containerStyles}
+      containerStyle={isDesktop ? containerStyles : mobileStyles}
       onDragend={onDrag}
       initialCenter={{
         lat: 37.799944,
